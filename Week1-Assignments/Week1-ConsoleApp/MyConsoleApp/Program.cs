dff9f79f8b2be0fff08916f195b41c79b51676e6
MacPerkins <macdperkins@gmail.com>
2024-11-06 23:03:58 -0700

    added w1 assignment minus API activity

---
+++ Week1-Assignments/Week1-ConsoleApp/MyConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!\n");
+Console.WriteLine($"The current time is {DateTime.Now}\n");
+
+DateTime targetDate = new DateTime(2024, 12, 25);
+int daysUntil = (targetDate - DateTime.Now).Days;
+Console.WriteLine($"Days left until Christmas: {daysUntil}\n");
